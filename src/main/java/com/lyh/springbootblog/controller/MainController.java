de609ee39c186a2e31239920d1279329a1bfef71
Van <734130098@qq.com>
2018-10-09 13:55:53 +0800

    完成注册和编辑页面的前后端

--- src/main/java/com/lyh/springbootblog/controller/MainController.java
+++ src/main/java/com/lyh/springbootblog/controller/MainController.java
@@ -1,8 +1,14 @@
 package com.lyh.springbootblog.controller;
 
+import com.lyh.springbootblog.domain.User;
+import com.lyh.springbootblog.service.UserService;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
 /**
  * Created with IntelliJ IDEA.
  * User: Van
@@ -14,6 +20,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class MainController {
 
+    @Autowired
+    UserService userService;
     /**
      * 根跳转
      * @return
@@ -60,5 +68,19 @@ public class MainController {
     @GetMapping("/register")
     public String register() {
         return  "register";
+    }
+
+    /**
+     * 注册用户
+     * @param user
+     * @return
+     */
+    @PostMapping("/register")
+    public String registerUser(User user) {
+        /*List<Authority> authorities = new ArrayList<>();
+        authorities.add(authorityService.getAuthorityById(ROLE_USER_AUTHORITY_ID));
+        user.setAuthorities(authorities);*/
+        userService.saveOrUpdateUser(user);
+        return "redirect:/login";
     }
 }
